82d9c061ee5a0e7e394276378995d08508389a7d
EvanJ0hnson <ivangerasimow@gmail.com>
2020-10-09 16:08:29 +0300

    feat(IDevice) Add "status" and "tags" field

--- src/entities/device.ts
+++ src/entities/device.ts
@@ -1,7 +1,10 @@
 import { IBaseEntity } from '../entities/base';
+import { EntityStatus } from './entity-status';
 
 export interface IDevice extends IBaseEntity {
   friendlyName?: string;
   description?: string;
   apps?: string[];
+  status: EntityStatus;
+  tags?: string[];
 }
